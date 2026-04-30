f1a3f39f41b640bbdb1b7a2b92e3613851ed4e9d
Cindy Pérez <tataperezu@gmail.com>
2026-04-29 23:54:26 -0500

    Revert "Merge pull request #9 from hrm-cesde-project/feature/e1-recruitment"
    This reverts commit 589db60aa51c93acefc1f490cf0446a7dcd05b0a, reversing
    changes made to aea4349d13cafecf08aefc156c5fa26376c5a7ae.

--- recruitment/src/main/java/co/edu/cesde/recruitment/application/port/input/CandidateServicePort.java
+++
@@ -1,9 +0,0 @@
-package co.edu.cesde.recruitment.application.port.input;
-
-import co.edu.cesde.recruitment.application.dto.CandidateCmd;
-
-
-public interface CandidateServicePort {
-
-	void createCandidate(CandidateCmd cmd);
-}
--- recruitment/src/main/java/co/edu/cesde/recruitment/infrastructure/persistence/CandidateJpaRepo.java
+++
@@ -1,7 +0,0 @@
-package co.edu.cesde.recruitment.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateJpaRepo extends JpaRepository<CandidateJpaEntity, Long> {
-
-}
