190d6efd7a857440bbe61d4cc973464c52c1bc5e
Esteban Velandia <estebanvelandia54@gmail.com>
2026-05-05 13:32:50 -0500

    feat/implementing-application

---
+++ selection/src/main/java/co/edu/cesde/selection/application/port/output/ContratacionNotifPort.java
@@ -0,0 +1,8 @@
+
+package co.edu.cesde.selection.application.port.output;
+
+    public interface ContratacionNotifPort {
+
+     public void notificarAprobado(Long aspiranteId, String cargo);
+    }
+
