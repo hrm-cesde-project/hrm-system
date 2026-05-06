fa78cde973ea4b4eb5af2fc63550a9eb66ac6040
Simon Dario Suarez Ortiz <ssuarezda@cesde.net>
2026-05-05 19:24:59 -0500

    feat: implementar estructura inicial del modulo de nomina

---
+++ payroll/src/main/java/co/edu/cesde/payroll/application/outputport/EmployeConsultPort.java
@@ -0,0 +1,4 @@
+package co.edu.cesde.payroll.application.outputport;
+
+public interface EmployeConsultPort {
+}
---
+++ payroll/src/main/java/co/edu/cesde/payroll/infrastructure/out/mapper/PayrrolMapper.java
@@ -0,0 +1,7 @@
+package co.edu.cesde.payroll.infrastructure.out.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface PayrrolMapper {
+}
