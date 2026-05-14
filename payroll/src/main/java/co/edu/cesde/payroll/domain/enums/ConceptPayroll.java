fba9b55e4fc0ca4726e3ab7864cbaef7d38a0172
edison_carmona <orlandocarmona2047@gmail.com>
2026-05-13 20:47:47 -0500

    feat: setup inicial de nomina entidades jpa y enums

--- payroll/src/main/java/co/edu/cesde/payroll/domain/enums/ConceptPayroll.java
+++ payroll/src/main/java/co/edu/cesde/payroll/domain/enums/ConceptPayroll.java
@@ -1,4 +1,11 @@
 package co.edu.cesde.payroll.domain.enums;
 
 public enum ConceptPayroll {
+    BASIC_SALARY,       // SALARIO_BASICO
+    TRANSPORT_ALLOWANCE, // AUXILIO_TRANSPORTE (if salary < 2 SMMLV) [cite: 14, 31]
+    OVERTIME,           // HORAS_EXTRA
+    HEALTH_INSURANCE,   // SALUD (4% deduction) [cite: 14, 31]
+    PENSION_FUND,       // PENSION (4% deduction) [cite: 14, 31]
+    WITHHOLDING_TAX     // RETENCION_FUENTE
+
 }
--- payroll/src/main/java/co/edu/cesde/payroll/domain/enums/TypeConcept.java
+++ payroll/src/main/java/co/edu/cesde/payroll/domain/enums/TypeConcept.java
@@ -1,4 +1,8 @@
 package co.edu.cesde.payroll.domain.enums;
 
 public enum TypeConcept {
+    EARNING,    // DEVENGADO: Sums to the employee's total [cite: 15, 30, 31]
+    DEDUCTION   // DEDUCCION: Subtracts from the employee's total [cite: 15, 30, 31]
+
+
 }
