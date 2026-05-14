c9aa793b4ce79f729f5f75dbe607dea0d0c69719
laura rojas <laurarojasmarin1104@gmail.com>
2026-05-13 20:03:51 -0500

    feat payrollServicePort, PayrollPersistencePort, PayrollUseCase

--- payroll/src/main/java/co/edu/cesde/payroll/application/outputport/PayrollPersistencePort.java
+++ payroll/src/main/java/co/edu/cesde/payroll/application/outputport/PayrollPersistencePort.java
@@ -1,4 +1,7 @@
 package co.edu.cesde.payroll.application.outputport;
 
+import co.edu.cesde.payroll.domain.Payroll;
+
 public interface PayrollPersistencePort {
+    Payroll save(Payroll payroll);
 }
--- payroll/src/main/java/co/edu/cesde/payroll/application/usecase/PayrollUseCase.java
+++ payroll/src/main/java/co/edu/cesde/payroll/application/usecase/PayrollUseCase.java
@@ -1,7 +1,19 @@
 package co.edu.cesde.payroll.application.usecase;
 
 import co.edu.cesde.payroll.application.inputport.PayrollServicePort;
+import co.edu.cesde.payroll.application.outputport.PayrollPersistencePort;
+import co.edu.cesde.payroll.domain.Payroll;
 
-public interface PayrollUseCase extends PayrollServicePort {
+public class PayrollUseCase implements PayrollServicePort {
 
+    private final PayrollPersistencePort payrollPersistencePort;
+
+    public PayrollUseCase(PayrollPersistencePort payrollPersistencePort) {
+        this.payrollPersistencePort = payrollPersistencePort;
+    }
+
+    @Override
+    public Payroll createPayroll(Payroll payroll) {
+        return payrollPersistencePort.save(payroll);
+    }
 }
