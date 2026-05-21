f9fe64e3434c56b1b7e8125fba7213c19e1d1ce9
Emacasbe <emacabeta@gmail.com>
2026-05-20 20:19:16 -0500

    se modifica TrainingPersistencePort, ProgramDTO y TrainingServicePort en Modulo Training

--- training/src/main/java/co/edu/cesde/training/application/dto/ProgramDTO.java
+++ training/src/main/java/co/edu/cesde/training/application/dto/ProgramDTO.java
@@ -1,4 +1,91 @@
 package co.edu.cesde.training.application.dto;
+import co.edu.cesde.training.domain.enums.Modality;
+import co.edu.cesde.training.domain.enums.ProgramType;
 
+import java.time.LocalDate;
 public class ProgramDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private ProgramType type;
+    private Modality modality;
+    private int durationHours;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private boolean active;
+
+    public ProgramDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProgramType getType() {
+        return type;
+    }
+
+    public void setType(ProgramType type) {
+        this.type = type;
+    }
+
+    public Modality getModality() {
+        return modality;
+    }
+
+    public void setModality(Modality modality) {
+        this.modality = modality;
+    }
+
+    public int getDurationHours() {
+        return durationHours;
+    }
+
+    public void setDurationHours(int durationHours) {
+        this.durationHours = durationHours;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
--- training/src/main/java/co/edu/cesde/training/application/outputport/TrainingPersistencePort.java
+++ training/src/main/java/co/edu/cesde/training/application/outputport/TrainingPersistencePort.java
@@ -1,5 +1,31 @@
 package co.edu.cesde.training.application.outputport;
+import co.edu.cesde.training.application.dto.ProgramDTO;
+import co.edu.cesde.training.domain.model.*;
 
+import java.util.List;
+import java.util.Optional;
 public interface TrainingPersistencePort {
+    TrainingProgram saveProgram(TrainingProgram program);
 
+    Enrollment saveEnrollment(Enrollment enrollment);
+
+    Certification saveCertification(Certification certification);
+
+    Optional<TrainingProgram> findProgramById(Long id);
+
+    Optional<Enrollment> findEnrollmentById(Long id);
+
+    List<Enrollment> findByEmployeeId(Long id);
+
+    List<TrainingProgram> findActivePrograms();
+
+    List<TrainingProgram> findAllPrograms();
+
+    List<ProgramDTO> getPrograms();
+
+    Optional<Certification> findCertificationById(Long id);
+
+    List<Certification> getCertifications();
+
+    List<Enrollment> findAllEnrollments();
 }
