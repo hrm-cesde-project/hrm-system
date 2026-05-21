0e7ba74d8f41203aa1d7ebb24c1a963e496f78d5
Adrian Jimenez <AdrianJimenez>
2026-05-20 20:27:49 -0500

    feat: Se crea completamente TrainingController, TrainingPersistenceAdapter y TrainingMapper

--- training/src/main/java/co/edu/cesde/training/infrastructure/mapper/TrainingMapper.java
+++ training/src/main/java/co/edu/cesde/training/infrastructure/mapper/TrainingMapper.java
@@ -1,7 +1,60 @@
 package co.edu.cesde.training.infrastructure.mapper;
+
+import co.edu.cesde.training.application.dto.CertificationDTO;
+import co.edu.cesde.training.application.dto.EnrollmentDTO;
+import co.edu.cesde.training.application.dto.ProgramDTO;
+import co.edu.cesde.training.domain.model.Certification;
+import co.edu.cesde.training.domain.model.Enrollment;
+import co.edu.cesde.training.domain.model.TrainingProgram;
+import co.edu.cesde.training.infrastructure.persistence.CertificationJpaEntity;
+import co.edu.cesde.training.infrastructure.persistence.EnrollmentJpaEntity;
+import co.edu.cesde.training.infrastructure.persistence.ProgramJpaEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface TrainingMapper {
 
+    /*
+    =========================
+    PROGRAM
+    =========================
+    */
+
+    ProgramDTO toDTO(TrainingProgram program);
+
+    TrainingProgram toDomain(ProgramDTO dto);
+
+    TrainingProgram toDomain(ProgramJpaEntity entity);
+
+    ProgramJpaEntity toEntity(TrainingProgram domain);
+
+    /*
+    =========================
+    ENROLLMENT
+    =========================
+    */
+
+    // ENTITY → DOMAIN
+    Enrollment toDomain(EnrollmentJpaEntity entity);
+
+    // DOMAIN → ENTITY
+    EnrollmentJpaEntity toEntity(Enrollment domain);
+
+    // DTO → DOMAIN  (ESTO ES CLAVE)
+    Enrollment toDomain(EnrollmentDTO dto);
+
+    // DOMAIN → DTO
+    EnrollmentDTO toEnrollmentDTO(Enrollment domain);
+
+    /*
+    =========================
+    CERTIFICATION
+    =========================
+    */
+
+    CertificationDTO toCertificationDTO(Certification certification);
+
+    Certification toDomain(CertificationJpaEntity entity);
+
+    CertificationJpaEntity toEntity(Certification domain);
 }
